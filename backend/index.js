2e2e2b7ac3765eb24111292e1075778c0071e04d
Chakibceran22 <grabachakib008@gmail.com>
2025-04-10 01:51:02 +0100

    making backed ready to reacive data from it

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,15 @@ import mqtt from 'mqtt';
 import { MQTT_TOPIC, MQTT_PORT, MQTT_HOST } from './consfig.js';
 
 const client = mqtt.connect(`mqtt://${MQTT_HOST}:${MQTT_PORT}`);
-
 client.on('connect', () => {
-  console.log('✅ Connected');
-  setInterval(() => {
-    const data = {
-      temperature: (20 + Math.random() * 5).toFixed(2),
-      time: new Date().toISOString()
-    };
-    client.publish(MQTT_TOPIC, JSON.stringify(data));
-    console.log('📤 Published:', data);
-  }, 3000);
+  client.subscribe('sensor/data', (err) => {
+    if (err) {
+      console.log('Error subscribing to topic', err);
+    }
+  });
+});
+client.on('message', (topic, message) => {
+  const sensorData = JSON.parse(message.toString());
+  console.log('Received sensor data:', sensorData);
+  // Push data to Firebase
 });
